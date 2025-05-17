90d3d22d89b78669a59f214226568624be7f9746
lucasugiette@hotmail.com <@@Bc0g0g39>
2025-05-17 00:38:20 -0300

    Initial commit

---
+++ src/main/java/com/puzzle/ezypayments/EzyPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.puzzle.ezypayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzyPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EzyPaymentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/puzzle/ezypayments/EzyPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puzzle.ezypayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzyPaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
